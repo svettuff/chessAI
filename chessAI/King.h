e81bc694aee3e2fb392f963fd25dc26d605a59ea
Богдан Ільніцький <bdancik8@gmail.com>
2025-05-18 00:07:06 +0300

    class constructor

--- chessAI/King.h
+++ chessAI/King.h
@@ -1,7 +1,10 @@
 ﻿#pragma once
+#include "Piece.h"
 
-class King
+class King : public Piece
 {
 public:
-    
+    King(int r, int c, int i);
+
+    std::vector<std::pair<int, int>> GetPossibleMoves() override;
 };
