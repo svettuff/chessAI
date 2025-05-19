75f89c766203ae836e022b6f2d24971fe71322ab
Богдан Ільніцький <bdancik8@gmail.com>
2025-05-20 00:14:38 +0300

    Queen GetPossibleMoves

--- chessAI/Queen.cpp
+++ chessAI/Queen.cpp
@@ -1,1 +1,18 @@
 ﻿#include "Queen.h"
+
+Queen::Queen(int r, int c, int i) : Piece(r, c, i)
+{
+    if (index < 16)
+        sign = u8"\u2655"; // White Queen
+    else 
+        sign = u8"\u265B"; // Black Queen
+}
+
+std::vector<std::pair<int, int>> Queen::GetPossibleMoves()
+{
+    std::vector<std::pair<int, int>> moves;
+
+
+
+    return moves;
+}
