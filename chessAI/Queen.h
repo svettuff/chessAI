f1bf6c187ecfb96dd2ae7126c895048c78ffcaaf
Богдан Ільніцький <bdancik8@gmail.com>
2025-05-20 00:03:39 +0300

    Add Queen class

---
+++ chessAI/Queen.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include "Piece.h"
+
+class Queen : public Piece
+{
+public:
+    Queen(int r, int c, int i);
+
+    std::vector<std::pair<int, int>> GetPossibleMoves() override;
+};
+
