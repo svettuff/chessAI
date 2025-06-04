e82f28f622f35a76acacdc68557bdb65c16a9d55
Богдан Ільніцький <bdancik8@gmail.com>
2025-06-04 12:20:59 +0300

    Rook GetPossibleMoves

---
+++ chessAI/Rook.cpp
@@ -0,0 +1,37 @@
+﻿#include "Rook.h"
+
+Rook::Rook(int r, int c, int i) : Piece(r, c, i)
+{
+    if (index < 16)
+        sign = u8"\u2656";  
+    else
+        sign = u8"\u265C";
+}
+
+std::vector<std::pair<int, int>> Rook::GetPossibleMoves()
+{
+    std::vector<std::pair<int, int>> moves;
+    
+    const std::vector<std::pair<int, int>> directions = {
+        {-1, 0},  
+        {1, 0},  
+        {0, -1},  
+        {0, 1}    
+    };
+
+    for (const auto& dir : directions)
+    {
+        for (int step = 1; step < 8; ++step)
+        {
+            int newRow = row + dir.first * step;
+            int newCol = col + dir.second * step;
+            
+            if (newRow >= 0 && newRow < 8 && newCol >= 0 && newCol < 8)
+                moves.emplace_back(newRow, newCol);
+            else
+                break;
+        }
+    }
+
+    return moves;
+}
---
+++ chessAI/Rook.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include "Piece.h"
+
+class Rook : public Piece
+{
+public:
+    Rook(int r, int c, int i);
+
+    std::vector<std::pair<int, int>> GetPossibleMoves() override;
+};
